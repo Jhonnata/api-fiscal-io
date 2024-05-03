ab754b12c9561cd6b8b54fb858b8cfd62480981d
Jhonnata <jhonnatasimoesdasilva@gmail.com>
2024-05-03 13:37:08 -0300

    feat: Changed "ShouldANumberInFull" for better coverage

--- tests/NumberInWords/NumberInWordsTest.php
+++ tests/NumberInWords/NumberInWordsTest.php
@@ -14,13 +14,27 @@ final class NumberInWordsTest extends CIUnitTestCase
      * @throws RedirectException
      */
     public function testShouldANumberInFull(){
-        $request = $this->get('/number-in-words', ['number'=>'96']);
-        $this->assertTrue($request->getJSON() !== false);
-        $request->assertJSONFragment(['text' => 'noventa e seis']);
+
 
         $request = $this->get('/number-in-words/2356');
         $this->assertTrue($request->getJSON() !== false);
         $request->assertJSONFragment(['text' => 'dois mil trezentos e cinquenta e seis']);
+
+        $request = $this->get('/number-in-words/356');
+        $this->assertTrue($request->getJSON() !== false);
+        $request->assertJSONFragment(['text' => 'trezentos e cinquenta e seis']);
+
+        $request = $this->get('/number-in-words', ['number'=>'96']);
+        $this->assertTrue($request->getJSON() !== false);
+        $request->assertJSONFragment(['text' => 'noventa e seis']);
+
+        $request = $this->get('/number-in-words/11');
+        $this->assertTrue($request->getJSON() !== false);
+        $request->assertJSONFragment(['text' => 'onze']);
+
+        $request = $this->get('/number-in-words/5');
+        $this->assertTrue($request->getJSON() !== false);
+        $request->assertJSONFragment(['text' => 'cinco']);
 
     }
 
